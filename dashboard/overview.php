ed4a5d62e293b945c50d1937e650442bdaf45f37
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-09 20:10:03 +0000

    New tag: Release 0.3.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/tags/0.3.0@124220 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dashboard/overview.php
@@ -0,0 +1,24 @@
+<?php 
+$aryConf['data'] = $this->call_API(
+			'VisitsSummary.get',
+			$aryConf['params']['period'],
+                        $aryConf['params']['date'],
+                        $aryConf['params']['limit']
+		);
+$aryConf['title'] = __('Overview', 'wp-piwik');
+include('header.php');
+?>
+<div class="table">
+<table class="widefat">
+<tbody>
+<?php
+$strTime = floor($aryConf['data']['sum_visit_length']/3600).'h '.floor(($aryConf['data']['sum_visit_length'] % 3600)/60).'m '.floor(($aryConf['data']['sum_visit_length'] % 3600) % 60).'s';
+echo '<tr><td>'.__('Visitors', 'wp-piwik').':</td><td>'.$aryConf['data']['nb_visits'].'</td></tr>';
+echo '<tr><td>'.__('Unique visitors', 'wp-piwik').':</td><td>'.$aryConf['data']['nb_uniq_visitors'].'</td></tr>';
+echo '<tr><td>'.__('Page views', 'wp-piwik').':</td><td>'.$aryConf['data']['nb_actions'].'</td></tr>';
+echo '<tr><td>'.__('Max. page views in one visit', 'wp-piwik').':</td><td>'.$aryConf['data']['max_actions'].'</td></tr>';
+echo '<tr><td>'.__('Total time spent by visitors', 'wp-piwik').':</td><td>'.$strTime.'</td></tr>';
+echo '<tr><td>'.__('Bounce count', 'wp-piwik').':</td><td>'.$aryConf['data']['bounce_count'].'</td></tr>';
+echo '</tbody></table>';
+?></div>
+<?php include('footer.php');
---
+++ js/wp-piwik.js
@@ -0,0 +1,7 @@
+
+jQuery( function($) {
+
+// close postboxes that should be closed
+jQuery('.if-js-closed').removeClass('if-js-closed').addClass('closed');
+postboxes.add_postbox_toggles('wppiwik');
+} );
