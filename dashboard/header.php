e3d6550dbb926be390df26dab78504a7d888c3ff
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-12 01:05:37 +0000

    WordPress 2.8 ready
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/tags/0.3.1@124996 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dashboard/header.php
@@ -0,0 +1,15 @@
+<?php
+/*********************************
+	WP-Piwik::Stats:Header
+**********************************/
+?>
+<div id="<?php echo $aryConf['id']; ?>" class="postbox <?php echo ($aryConf['closed']?'if-js-closed':''); ?>" >
+	<div class="handlediv" title="Zum Umschalten klicken">
+		<br />
+	</div>
+	<h3 class='hndle'>
+		<span>
+			<?php echo $aryConf['title']; echo ' <span class="wp-piwik-details">('.$aryConf['desc'].')</span>'; ?>
+		</span>
+	</h3>
+	<div class="inside">
