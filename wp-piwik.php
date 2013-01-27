eab0c108c0cb36a35204dddeeed86b479370b9c3
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-27 13:41:05 +0000

    Fix regarding wpdb::prepare: https://make.wordpress.org/core/2012/12/12/php-warning-missing-argument-2-for-wpdb-prepare/
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/trunk@659678 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-piwik.php
+++ wp-piwik.php
@@ -1354,7 +1354,7 @@ class wp_piwik {
 		// Reset network settings
 		if (is_plugin_active_for_network('wp-piwik/wp-piwik.php')) {
 			delete_site_option('wp-piwik_global-settings');
-			$aryBlogs = $wpdb->get_results($wpdb->prepare('SELECT blog_id FROM '.$wpdb->blogs.' ORDER BY blog_id'));
+			$aryBlogs = $wpdb->get_results($wpdb->prepare('SELECT blog_id FROM %s ORDER BY blog_id', $wpdb->blogs));
 			foreach ($aryBlogs as $aryBlog)
 				delete_blog_option($aryBlog->blog_id, 'wp-piwik_settings');
 			update_site_option('wp-piwik_global-settings', $aryKeep);
