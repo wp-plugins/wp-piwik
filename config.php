1cb3c90c43bf6420911ab30ae2ed7514789e9688
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-05 19:22:34 +0000

    = 0.9.9.3 = * Sparkline script update (IE 10 compatbility) * Syntax error fixes
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/tags/0.9.9.3@708307 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	/*
+		Configure WP-Piwik Logger
+		0: Logger disabled
+		1: Log to screen (available soon)
+		2: Log to file (logs/YYYYMMDD_wp-piwik.log)
+	*/
+	const WP_PIWIK_ACTIVATE_LOGGER = 0;
