e2dd1bd3fb9b802ab6ee529c81cd7cea7c051487
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-19 21:46:56 +0000

    0.9.9.4 release - Caching added, rec=1 option added
    Bugfix: Don't cache tracking code
    
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/tags/0.9.9.4@715363 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+	/*
+		Configure WP-Piwik Logger
+		0: Logger disabled
+		1: Log to screen (available soon)
+		2: Log to file (logs/YYYYMMDD_wp-piwik.log)
+	*/
+	const WP_PIWIK_ACTIVATE_LOGGER = 0;
+	const WP_PIWIK_ACTIVATE_CACHE = true;
