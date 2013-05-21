eecf3ab6fbe815d4f7c0f3f282e439a755fb0bcd
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-21 20:55:16 +0000

    Bugfix: Fatal error on settings/views.php
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/tags/0.9.9.5@716227 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+	/*
+		Configure WP-Piwik Logger
+		0: Logger disabled
+		1: Log to screen (available soon)
+		2: Log to file (logs/YYYYMMDD_wp-piwik.log)
+	*/
+	define('WP_PIWIK_ACTIVATE_LOGGER', 0);
+	define('WP_PIWIK_ACTIVATE_CACHE', true);
