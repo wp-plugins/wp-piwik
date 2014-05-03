7e3fb4a268a5929ad495bbc2e854203e9ec45095
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-03 19:29:56 +0000

    0.9.9.10 release; rollback + new PHP ver check
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/tags/0.9.9.10@907511 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- phpapi.php
+++ phpapi.php
@@ -1,13 +1,5 @@
 <?php
 
-		if (!defined('PIWIK_INCLUDE_PATH'))
-			return;
-		if (PIWIK_INCLUDE_PATH === FALSE)
-			return serialize(array('result' => 'error', 'message' => __('Could not resolve','wp-piwik').' &quot;'.htmlentities(self::$settings->getGlobalOption('piwik_path')).'&quot;: '.__('realpath() returns false','wp-piwik').'.'));
-		if (file_exists(PIWIK_INCLUDE_PATH . "/index.php"))
-			require_once PIWIK_INCLUDE_PATH . "/index.php";
-		if (file_exists(PIWIK_INCLUDE_PATH . "/core/API/Request.php"))
-			require_once PIWIK_INCLUDE_PATH . "/core/API/Request.php";
 		if (class_exists('Piwik\FrontController'))
 			Piwik\FrontController::getInstance()->init();
 		else serialize(array('result' => 'error', 'message' => __('Class Piwik\FrontController does not exists.','wp-piwik')));
