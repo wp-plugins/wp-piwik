662d21c9bacfa8c5012f284d253d7420beab3624
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-03 19:42:28 +0000

    Reset.
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/tags/0.9.9.10@907515 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- phpapi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-		if (class_exists('Piwik\FrontController'))
-			Piwik\FrontController::getInstance()->init();
-		else serialize(array('result' => 'error', 'message' => __('Class Piwik\FrontController does not exists.','wp-piwik')));
-		if (class_exists('Piwik\API\Request'))
-			$objRequest = new Piwik\API\Request($strParams);
-		else serialize(array('result' => 'error', 'message' => __('Class Piwik\API\Request does not exists.','wp-piwik')));
