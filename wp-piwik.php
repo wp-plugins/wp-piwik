84181432d923342a4d94444f8c2cceda99dd4f4e
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-28 21:03:21 +0000

    Bugfix
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/tags/0.9.9.11@922743 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-piwik.php
+++ wp-piwik.php
@@ -741,6 +741,9 @@ class wp_piwik {
 			return;
 		if (PIWIK_INCLUDE_PATH === FALSE)
 			return serialize(array('result' => 'error', 'message' => __('Could not resolve','wp-piwik').' &quot;'.htmlentities(self::$settings->getGlobalOption('piwik_path')).'&quot;: '.__('realpath() returns false','wp-piwik').'.'));
+		$current = ob_get_contents();
+		ob_end_clean();
+		ob_start();
 		if (file_exists(PIWIK_INCLUDE_PATH . "/index.php"))
 			require_once PIWIK_INCLUDE_PATH . "/index.php";
 		if (file_exists(PIWIK_INCLUDE_PATH . "/core/API/Request.php"))
@@ -751,6 +754,9 @@ class wp_piwik {
 		if (class_exists('Piwik\API\Request'))
 			$objRequest = new Piwik\API\Request($strParams);
 		else serialize(array('result' => 'error', 'message' => __('Class Piwik\API\Request does not exists.','wp-piwik')));
+		ob_end_clean();
+		ob_start;
+		echo $current;
 		return $objRequest->process();		
 	}
 
