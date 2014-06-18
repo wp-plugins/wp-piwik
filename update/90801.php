a2b349527811e36cf03c90cf70bc943ea8a81954
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-18 20:18:46 +0000

    Remove deprecated update files
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/trunk@934699 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- update/90801.php
+++ update/90801.php
@@ -1,6 +1,4 @@
 <?php
-self::$settings->setGlobalOption('track_search', false);
-self::$settings->setGlobalOption('connection_timeout', 5);
 if (self::$settings->getGlobalOption('track_compress'))
 	self::$settings->setGlobalOption('track_mode', 1);
 else 
