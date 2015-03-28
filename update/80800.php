815578dbab4a0ec8e95fe6b5f4e3505efb6e0de6
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-28 20:59:36 +0000

    Update files
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/tags/0.9.9.13@1122870 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- update/80800.php
+++ update/80800.php
@@ -1,2 +1,2 @@
 <?php
-self::$settings->setGlobalOption('piwik_url', self::$settings->getGlobalOption('piwik_url').((substr($strURL, -1, 1) != '/' && substr($strURL, -10, 10) != '/index.php')?'/':''));
+self::$settings->setGlobalOption('piwik_url', self::checkURL(self::$settings->getGlobalOption('piwik_url')));
--- update/90801.php
+++ update/90801.php
@@ -1,4 +1,6 @@
 <?php
+self::$settings->setGlobalOption('track_search', false);
+self::$settings->setGlobalOption('connection_timeout', 5);
 if (self::$settings->getGlobalOption('track_compress'))
 	self::$settings->setGlobalOption('track_mode', 1);
 else 
