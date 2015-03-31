9dc21e3e8e5aa3db9d5bce56b5cb387a706005b6
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-31 12:03:04 +0000

    Bitcoin link
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/tags/0.9.9.15@1124400 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-piwik.php
+++ wp-piwik.php
@@ -1325,8 +1325,8 @@ class wp_piwik {
 				</form>
 			</div>
 			<div>
-				<img src="<?php echo $this->getPluginURL(); ?>bitcoin.png" width="125" height="125" alt="Bitcoin Address" title="3N8od4UQA3jDpP5KodkxSqENaRSMWEaTHp" /><br />
-				<small>Bitcoin address: 3N8od4UQA3jDpP5KodkxSqENaRSMWEaTHp</small>
+				<a href="bitcoin:3N8od4UQA3jDpP5KodkxSqENaRSMWEaTHp">Bitcoin
+				<img style="border:none;" src="<?php echo $this->getPluginURL(); ?>bitcoin.png" width="125" height="125" alt="Bitcoin Address" title="3N8od4UQA3jDpP5KodkxSqENaRSMWEaTHp" /></a>
 			</div>
 			<div>
 				<a href="http://www.amazon.de/gp/registry/wishlist/111VUJT4HP1RA?reveal=unpurchased&amp;filter=all&amp;sort=priority&amp;layout=standard&amp;x=12&amp;y=14"><?php _e('My Amazon.de wishlist', 'wp-piwik'); ?></a>
