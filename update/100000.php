7cbf4d27c0f770f547b14f80fe10a3eff589f311
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-02 23:51:40 +0000

    - Install script - Manual script for network sites
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/trunk@1152006 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ update/100000.php
@@ -0,0 +1,18 @@
+<?php
+	// Get & delete old version's options
+	if (self::$settings->checkNetworkActivation()) {
+		$oldGlobalOptions = get_site_option('wp-piwik_global-settings', array());
+		delete_site_option('wp-piwik_global-settings');
+	} else {
+		$oldGlobalOptions = get_option('wp-piwik_global-settings', array());
+		delete_option('wp-piwik_global-settings');
+	}
+	$oldOptions = get_option('wp-piwik_settings', array());
+	delete_option('wp-piwik_settings');
+	
+	// Store old values in new settings
+	foreach ($oldGlobalOptions as $key => $value)
+		self::$settings->setGlobalOption($key, $value);
+	foreach ($oldOptions as $key => $value)
+		self::$settings->setOption($key, $value);
+	self::$settings->save();
