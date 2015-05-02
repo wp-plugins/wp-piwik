803717402a01d1d0d8f7c24a585e186db1d1fd9d
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-02 20:45:44 +0000

    - Proxy script fix (will work with PHP mode & Pro now) - Caching functionality added
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/trunk@1151953 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/WP_Piwik/Request.php
+++ classes/WP_Piwik/Request.php
@@ -4,7 +4,7 @@
 
 	abstract class Request {
 		
-		protected static $wpPiwik, $settings, $requests = array(), $results = array(), $piwikVersion;
+		protected static $wpPiwik, $settings, $requests = array(), $results = array(), $isCacheable = array(), $piwikVersion;
 		
 		public function __construct($wpPiwik, $settings) {
 			self::$wpPiwik = $wpPiwik;
@@ -17,11 +17,19 @@
 				$id = 'global.getPiwikVersion';
 			else
 				$id = 'method='.$method.self::parameterToString($parameter);
+			if ( 
+				in_array( $method, array( 'API.getPiwikVersion', 'SitesManager.getJavascriptTag', 'SitesManager.getAllSites', 'SitesManager.getSitesIdFromSiteUrl', 'SitesManager.addSite', 'SitesManager.updateSite' ) ) ||
+				substr($parameter['date'], 0, 4) == 'last' ||
+				$parameter['date'] == 'today' ||
+				( $parameter['period'] == 'day' && $parameter['date'] == date('Ymd') ) ||
+				( $parameter['period'] == 'month' && $parameter['date'] == date('Ym') ) ||
+				( $parameter['period'] == 'week' && $parameter['date'] == date( 'Ymd', strtotime( "last Monday" ) ) ) 
+			) self::$isCacheable[$id] = false;
+			else self::$isCacheable[$id] = $method.'-'.$parameter['period'].'-'.$parameter['date'];
 			if (!isset(self::$requests[$id]))
 				self::$requests[$id] = array('method' => $method, 'parameter' => $parameter);
 			return $id;
 		}
-		
 		private static function parameterToString($parameter) {
 			$return = '';
 			if (is_array($parameter))
@@ -31,13 +39,22 @@
 		}
 		
 		public function perform($id) {
+			if ( self::$settings->getGlobalOption('cache') && false !== ( $cached = get_transient( 'wp-piwik_c_'.md5(self::$isCacheable[$id] ) ) ) ) {
+				self::$wpPiwik->log("Deliver cached data: ".$id);
+				return $cached;
+			}
 			self::$wpPiwik->log("Perform request: ".$id);
 			if (!isset(self::$requests[$id]))
 				return array('result' => 'error', 'message' => 'Request '.$id.' was not registered.');
 			elseif (!isset(self::$results[$id])) {
 				$this->request($id);
 			}
-			return isset(self::$results[$id])?self::$results[$id]:false;
+			if ( isset ( self::$results[$id] )) {
+				if ( self::$settings->getGlobalOption('cache') && self::$isCacheable[$id] ) {
+					set_transient( 'wp-piwik_c_'.md5(self::$isCacheable[$id]) , self::$results[$id], WEEK_IN_SECONDS );
+				}
+				return self::$results[$id];
+			} else return false;
 		}
 		
 		protected function buildURL($config, $urlDecode = false) {
--- proxy/config.php
+++ proxy/config.php
@@ -8,7 +8,18 @@ require_once('../classes/WP_Piwik/Logger/Dummy.php');
 $logger = new WP_Piwik\Logger\Dummy(__CLASS__);
 $settings = new WP_Piwik\Settings($logger);
 
-$PIWIK_URL = $settings->getGlobalOption('piwik_mode') == 'php'?$settings->getGlobalOption('proxy_url'):$settings->getGlobalOption('piwik_url');
+$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off')?'https':'http';
+
+switch ( $settings->getGlobalOption('piwik_mode') ) {
+	case 'php':
+		$PIWIK_URL = $protocol.':'.$settings->getGlobalOption('proxy_url');
+	break;
+	case 'pro':
+		$PIWIK_URL = 'https://'.$settings->getGlobalOption('piwik_user').'.piwik.pro/';
+	break;
+	default:
+		$PIWIK_URL = $settings->getGlobalOption('piwik_url');
+}
 
 if (substr($PIWIK_URL, 0, 2) == '//')
 	$PIWIK_URL = (isset($_SERVER['HTTPS'])?'https:':'http:').$PIWIK_URL;
@@ -16,4 +27,4 @@ if (substr($PIWIK_URL, 0, 2) == '//')
 $TOKEN_AUTH = $settings->getGlobalOption('piwik_token');
 $timeout = $settings->getGlobalOption('connection_timeout');
 
-ini_set('display_errors',1);
+ini_set('display_errors',0);
