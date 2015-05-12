5662fabe5723c7cfd36e364838b8a97215bed582
Braekling <Braekling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 06:17:01 +0000

    Remove update notice if update already performed: https://wordpress.org/support/topic/admin-message-does-not-disapear
    git-svn-id: https://plugins.svn.wordpress.org/wp-piwik/trunk@1158460 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- update/2015051101.php
+++ update/2015051101.php
@@ -1,18 +1,31 @@
 <?php
+
+// Get & delete old version's options
+if (self::$settings->checkNetworkActivation ()) {
+	$oldGlobalOptions = get_site_option ( 'wp-piwik_global-settings', array () );
+	delete_site_option('wp-piwik_global-settings');
+} else {
+	$oldGlobalOptions = get_option ( 'wp-piwik_global-settings', array () );
+	delete_option('wp-piwik_global-settings');
+}
+
+$oldOptions = get_option ( 'wp-piwik_settings', array () );
+delete_option('wp-piwik_settings');
 	
+if (function_exists('is_multisite') && is_multisite()) {
+	global $wpdb;
+	$aryBlogs = $wpdb->get_results('SELECT blog_id FROM '.$wpdb->blogs.' ORDER BY blog_id');
+	if (is_array($aryBlogs))
+		foreach ($aryBlogs as $aryBlog) {
+			$oldOptions = get_blog_option ( $aryBlog->blog_id, 'wp-piwik_settings', array () );
+			if (!$this->isConfigured())
+				foreach ( $oldOptions as $key => $value )
+					self::$settings->setOption ( $key, $value, $aryBlog->blog_id );
+			delete_blog_option($aryBlog->blog_id, 'wp-piwik_settings');
+		}
+}
+
 if (!$this->isConfigured()) {
-	// Get & delete old version's options
-	if (self::$settings->checkNetworkActivation ()) {
-		$oldGlobalOptions = get_site_option ( 'wp-piwik_global-settings', array () );
-		delete_site_option('wp-piwik_global-settings');
-	} else {
-		$oldGlobalOptions = get_option ( 'wp-piwik_global-settings', array () );
-		delete_option('wp-piwik_global-settings');
-	}
-
-	$oldOptions = get_option ( 'wp-piwik_settings', array () );
-	delete_option('wp-piwik_settings');
-
 	if (!$oldGlobalOptions['add_tracking_code']) $oldGlobalOptions['track_mode'] = 'disabled';
 	elseif (!$oldGlobalOptions['track_mode']) $oldGlobalOptions['track_mode'] = 'default';
 	elseif ($oldGlobalOptions['track_mode'] == 1) $oldGlobalOptions['track_mode'] = 'js';
@@ -23,18 +36,6 @@ if (!$this->isConfigured()) {
 		self::$settings->setGlobalOption ( $key, $value );
 	foreach ( $oldOptions as $key => $value )
 		self::$settings->setOption ( $key, $value );
-
-	if (function_exists('is_multisite') && is_multisite()) {
-		global $wpdb;
-		$aryBlogs = $wpdb->get_results('SELECT blog_id FROM '.$wpdb->blogs.' ORDER BY blog_id');
-		if (is_array($aryBlogs))
-			foreach ($aryBlogs as $aryBlog) {
-				$oldOptions = get_blog_option ( $aryBlog->blog_id, 'wp-piwik_settings', array () );
-				foreach ( $oldOptions as $key => $value )
-					self::$settings->setOption ( $key, $value, $aryBlog->blog_id );
-				delete_blog_option($aryBlog->blog_id, 'wp-piwik_settings');
-			}
-	}
 }
 
 self::$settings->save ();
